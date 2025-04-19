ddc0215774d25c8c6da1bb0a5d931a40c49ada57
Aliaksandr Bykouski <developweb34@gmail.com>
2025-04-20 00:10:23 +0200

    add accordion

--- src/sections/Questions/Questions.jsx
+++ src/sections/Questions/Questions.jsx
@@ -1,19 +1,44 @@
 import Section from "@/layouts/Section";
 import Button from "@/components/Button";
 import AccordionGroup from "@/components/AccordionGroup";
+import Accordion from "@/components/Accordion/index.js";
 
 const Questions = () => {
 
   const questionItems = [
-    'What is StreamVibe?',
-    'How much does StreamVibe cost?',
-    'What content is available on StreamVibe?',
-    'How can I watch StreamVibe?',
-    'How do I sign up for StreamVibe?',
-    'What is the StreamVibe free trial?',
-    'How do I contact StreamVibe customer support?',
-    'What are the StreamVibe payment methods?'
-  ]
+    {
+      question: 'What is StreamVibe?',
+      answer: 'StreamVibe is a streaming service that allows you to watch movies and shows on demand.',
+    },
+    {
+      question: 'How much does StreamVibe cost?',
+      answer: 'StreamVibe offers several plans starting from $9.99 per month.',
+    },
+    {
+      question: 'What content is available on StreamVibe?',
+      answer: 'StreamVibe includes a wide range of movies, TV shows, and exclusive originals.',
+    },
+    {
+      question: 'How can I watch StreamVibe?',
+      answer: 'You can watch on your phone, tablet, smart TV, or computer using the StreamVibe app or website.',
+    },
+    {
+      question: 'How do I sign up for StreamVibe?',
+      answer: 'Simply visit our website and follow the sign-up process to create your account.',
+    },
+    {
+      question: 'What is the StreamVibe free trial?',
+      answer: 'StreamVibe offers a 7-day free trial for new users to explore our service.',
+    },
+    {
+      question: 'How do I contact StreamVibe customer support?',
+      answer: 'You can contact support through our Help Center or by emailing support@streamvibe.com.',
+    },
+    {
+      question: 'What are the StreamVibe payment methods?',
+      answer: 'We accept credit/debit cards, PayPal, and other popular payment methods.',
+    },
+  ];
 
   return (
     <Section
@@ -28,7 +53,17 @@ const Questions = () => {
       )}
     >
       <AccordionGroup columns={2}>
-        {questionItems}
+        {questionItems.map((item, index) => (
+          <Accordion
+            title={item.question}
+            id={`question-${index}`}
+            name='questions'
+            isOpen={index === 0}
+            key={index}
+            >
+            <p>{item.answer}</p>
+          </Accordion>
+        ))}
       </AccordionGroup>
     </Section>
   )
