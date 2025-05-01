6eb21b415075bc83ee55025cf148342fc6fa4e8d
Aliaksandr Bykouski <developweb34@gmail.com>
2025-05-01 11:11:27 +0200

    start page show, section banner

---
+++ src/pages/show.jsx
@@ -0,0 +1,14 @@
+import ShowBanner from "@/sections/ShowBanner/index.js";
+
+
+export const metadata = {
+  title: "Show - Stranger Things",
+}
+
+export default function () {
+  return (
+    <>
+      <ShowBanner />
+    </>
+  )
+}
